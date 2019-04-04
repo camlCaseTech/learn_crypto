88a927f95d134a91a9cf8b138b8ac877866b4915
James Haver <mchaver@gmail.com>
2019-04-04 10:52:54 +0200

    Setup react project using yarn

--- learn-crypto-frontend/src/App.js
+++ learn-crypto-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Thing from './Thing.js';
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Thing></Thing>
         </header>
       </div>
     );
